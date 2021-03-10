c12fcb4b3c80f1d74bb0a9bcb46e32276415302d
Avinash Kumar <avinashbest3@gmail.com>
2021-03-10 21:58:18 +0530

    initial commit

---
+++ gettingStarted/helloWorld.java
@@ -0,0 +1,10 @@
+package gettingStarted;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        // If we use println java will put a newline at the end
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+    }
+}
