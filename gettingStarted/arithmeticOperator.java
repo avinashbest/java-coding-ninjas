335bfd5c3865f02285fb47888e0eb1a276a5303f
Avinash Kumar <avinashbest3@gmail.com>
2021-03-14 13:31:50 +0530

    Completed Getting started with Java

---
+++ gettingStarted/arithmeticOperator.java
@@ -0,0 +1,19 @@
+package gettingStarted;
+
+public class arithmeticOperator {
+    public static void main(String[] args) {
+
+        System.out.println(9 + 5);
+        System.out.println(9 - 5);
+        System.out.println(9 / 5);
+        System.out.println(9 * 5);
+
+        System.out.println(9 % 5);
+
+        System.out.println('a' + 'b');
+
+        char c = 'a';
+        char i = (char) (c + 3);
+        System.out.println(i);
+    }
+}
