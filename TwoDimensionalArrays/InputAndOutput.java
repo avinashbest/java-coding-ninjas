d97e2b0fe3ba7a4be89efea32c1f175c249a1065
Avinash Kumar <avinashbest3@gmail.com>
2021-04-08 21:49:47 +0530

    2D-Arrays started!

---
+++ TwoDimensionalArrays/InputAndOutput.java
@@ -0,0 +1,27 @@
+package TwoDimensionalArrays;
+
+import java.util.Scanner;
+
+public class InputAndOutput {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int row = scan.nextInt();
+        System.out.println("Enter number of columns:");
+        int column = scan.nextInt();
+        int[][] arr = new int[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.println("Enter the element at " + i + "th rows " + j + "th column:");
+                arr[i][j] = scan.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
