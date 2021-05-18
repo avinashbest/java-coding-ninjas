2a319e4d072e1c5c024a097fbbac3458e769a1e5
Avinash Kumar <avinashbest3@gmail.com>
2021-05-18 23:43:41 +0530

    Getters & Setters

--- ojectOrientedProgramming1/StudentClass/Student.java
+++ ojectOrientedProgramming1/StudentClass/Student.java
@@ -1,6 +1,19 @@
 package ojectOrientedProgramming1.StudentClass;
 
 public class Student {
-    String name;
-    int rollNumber;
+    public String name;
+    private int rollNumber;
+
+    /*GETTERS*/
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    /*SETTERS*/
+    public void setRollNumber(int r) {
+        if (r <= 0) {
+            return;
+        }
+        rollNumber = r;
+    }
 }
--- ojectOrientedProgramming1/StudentClass/StudentUse.java
+++ ojectOrientedProgramming1/StudentClass/StudentUse.java
@@ -3,14 +3,9 @@ package ojectOrientedProgramming1.StudentClass;
 public class StudentUse {
     public static void main(String[] args) {
         Student s1 = new Student();
-        Student s2 = new Student();
 
         s1.name = "Avinash Kumar";
-        s1.rollNumber = 191041;
-        System.out.println("Name: " + s1.name + " Roll No.: " + s1.rollNumber);
-
-        s2.name = "Abhishek Rawe";
-        s2.rollNumber = 191012;
-        System.out.println("Name: " + s2.name + " Roll No.: " + s2.rollNumber);
+        s1.setRollNumber(191041);
+        System.out.println(s1.name + "\t" + s1.getRollNumber());
     }
 }
