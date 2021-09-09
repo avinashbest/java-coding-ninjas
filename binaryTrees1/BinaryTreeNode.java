46d9b90a0922b42e772d2454d150afa4f5a13b89
avinashbest <avinashbest3@gmail.com>
2021-09-09 15:57:03 +0530

    Binary Trees - I : Started

---
+++ binaryTrees1/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package binaryTrees1;
+
+public class BinaryTreeNode<T> {
+    public T data;
+    public BinaryTreeNode<T> left;
+    public BinaryTreeNode<T> right;
+
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
---
+++ binaryTrees1/BinaryTreeUse.java
@@ -0,0 +1,49 @@
+package binaryTrees1;
+
+public class BinaryTreeUse {
+
+    /* Root -> Left -> Right: Traversal on Binary Tree */
+    public static void printBinaryTree(BinaryTreeNode<Integer> root) {
+//        Base Case: if the root is null we just return from that position without doing anything
+        if (root == null) return;
+        System.out.println(root.data);
+        printBinaryTree(root.left);
+        printBinaryTree(root.right);
+    }
+
+    /* Root -> Left -> Right: Traversal on Binary Tree */
+    public static void printBinaryTreeDetailed(BinaryTreeNode<Integer> root) {
+//        Base Case: if the root is null we just return from that position without doing anything
+        if (root == null) return;
+//        At this point we only know root is not null
+        System.out.print(root.data + " : ");
+//        Printing for the root's left & right data
+        if (root.left != null) System.out.print("Left -> " + root.left.data + ", ");
+        if (root.right != null) System.out.print("Right -> " + root.right.data);
+        System.out.println();
+//        Now, calling recursively on root's left and root's right
+        printBinaryTreeDetailed(root.left);
+        printBinaryTreeDetailed(root.right);
+    }
+
+    public static void main(String[] args) {
+//        Creating the nodes
+        var root = new BinaryTreeNode<Integer>(1);
+
+        var rootLeft = new BinaryTreeNode<Integer>(2);
+        var rootRight = new BinaryTreeNode<Integer>(3);
+//        connecting the nodes
+        root.left = rootLeft;
+        root.right = rootRight;
+//        first part of tree created
+
+//        second part is creating
+        var twoRight = new BinaryTreeNode<Integer>(4);
+        var threeLeft = new BinaryTreeNode<Integer>(5);
+
+        rootLeft.right = twoRight;
+        rootRight.left = threeLeft;
+
+        printBinaryTreeDetailed(root);
+    }
+}
