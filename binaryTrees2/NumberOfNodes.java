f716b50a5ea17b0a27545651fb0abd4793063446
avinashbest <avinashbest3@gmail.com>
2021-09-14 14:54:06 +0530

    Binary Trees - II : Started

---
+++ binaryTrees2/NumberOfNodes.java
@@ -0,0 +1,22 @@
+package binaryTrees2;
+
+import binaryTrees1.BinaryTreeNode;
+
+import static binaryTrees1.BinaryTreeInput.printBinaryTree;
+import static binaryTrees1.BinaryTreeInput.takeTreeInputDetailed;
+
+public class NumberOfNodes {
+
+    public static int noOfNodes(BinaryTreeNode<Integer> root) {
+        if (root == null) return 0;
+        return 1 + noOfNodes(root.left) + noOfNodes(root.right);
+    }
+
+    public static void main(String[] args) {
+        var root = takeTreeInputDetailed(true, 0, true);
+        System.out.println("----------The Tree is----------");
+        printBinaryTree(root);
+        System.out.println("-------------------------------");
+        System.out.println("Number of Nodes = " + noOfNodes(root));
+    }
+}
